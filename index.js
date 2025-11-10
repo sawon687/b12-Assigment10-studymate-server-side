9b196210389b26a7a187e2b942b897f79ca55be9
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-11-10 18:43:34 +0600

    added mongodb conection and post data api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express=require('express');
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ofja8we.mongodb.net/?appName=Cluster0`;
 const app=express();
-const core=require('cors')
-const port=process.env.PORT || 3000
+const cors=require('cors')
+require('dotenv').config();
+const port=process.env.PORT || 9000
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
+@cluster0.ofja8we.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -15,7 +18,7 @@ const client = new MongoClient(uri, {
 });
 
 app.use(express.json())
-app.use(core())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('server start studymate')
@@ -25,6 +28,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db=client.db('StudymateDB')
+    const userProfileColl=db.collection('create-user-profile')
+
+    app.post('/user-Profile',async(req,res)=>{
+        const createProfile=req.body;
+        const result=await userProfileColl.insertOne(createProfile)
+        res.send(result)
+    })
+
+    app.get('/userProfile',async(req,res)=>{
+        const result=await userProfileColl.find().toArray()
+       res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -33,6 +50,7 @@ async function run() {
     
   }
 }
+console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD);
 
 run().catch(console.log(console.dir))
 app.listen(port,()=>{
