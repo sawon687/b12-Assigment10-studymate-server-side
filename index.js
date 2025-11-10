06cdd68b2087e9c418d4e663b485534ef4664f9e
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-11-10 17:02:01 +0600

    added instal mongodb  and useaddmin password username

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express=require('express');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ofja8we.mongodb.net/?appName=Cluster0`;
 const app=express();
 const core=require('cors')
 const port=process.env.PORT || 3000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.use(express.json())
 app.use(core())
@@ -10,6 +21,20 @@ app.get('/',(req,res)=>{
     res.send('server start studymate')
 })
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+
+run().catch(console.log(console.dir))
 app.listen(port,()=>{
     console.log('server start here studymate ',port)
 })
