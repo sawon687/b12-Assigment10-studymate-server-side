597c295750c0511a90297bdd6624f50653e2b94d
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-11-11 19:44:10 +0600

    added myconection post and delete  email query mycollection  and update patner count inceress

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     await client.connect();
     const db=client.db('StudymateDB')
     const userProfileColl=db.collection('create-user-profile')
+    const myConnection=db.collection('partnerRequest')
         // createProfile
     app.post('/createProfile',async(req,res)=>{
         const createProfile=req.body;
@@ -48,6 +49,46 @@ async function run() {
         const result=await userProfileColl.find().toArray()
        res.send(result)
     })
+      //  myconnection user
+      app.post('/myConnection',async(req,res)=>{
+        try {
+            const partnerRequest=req.body;
+         const {partnerId,request_Email}=partnerRequest;
+         const  query={_id: new ObjectId(partnerId)}
+          const alredayRequested=await myConnection.findOne({partnerId,request_Email})
+          
+          if(alredayRequested)
+          {
+             return res.send({message:'you have alreday request'})
+          }
+           const update={$inc:{patnerCount:+ 1}}
+          const updateCount=await userProfileColl.updateOne(query,update)
+           const result=await myConnection.insertOne(partnerRequest)
+          
+         
+         return res.send({
+          success: true,
+        message: "Partner Request Sent Successfully!",
+          requsetData:result,updateCount})
+         
+        } catch (error) {
+           return res.status(500).send({ success: false, message: error.message });
+        }
+        
+      })
+
+      // myconnection delete 
+
+     
+     
+    app.get('/myConnection',async(req,res)=>{
+      const  email=req.query.email;
+      const  query={request_Email:email}
+      const  result=await myConnection.find(query).toArray();
+      
+      res.send(result);
+    })
+    
     app.get('/topStudyProfile',async(req,res)=>{
         const result=await userProfileColl.find().sort({rating:-1}).limit(6).toArray()
         res.send(result)
@@ -66,3 +107,4 @@ run().catch(console.log(console.dir))
 app.listen(port,()=>{
     console.log('server start here studymate ',port)
 })
+
